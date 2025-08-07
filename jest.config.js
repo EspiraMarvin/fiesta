3a706772e6117d849f91378c938f604d469d1e66
EspiraMarvin <espiramarvin@gmail.com>
2025-08-07 16:28:21 +0300

    test: add unit tests
    - mocking redis client
    - mocking db calls - prisma mocks

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const { createDefaultPreset } = require('ts-jest');
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+};
